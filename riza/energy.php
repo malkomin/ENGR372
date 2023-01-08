b0600a337acd5882637fcdf5b748fd4176870b45
malkomin <mehmetaliuyar5@gmail.com>
2023-01-09 02:54:10 +0300

    fxi

--- riza/energy.php
+++ riza/energy.php
@@ -1,17 +1,17 @@
 <?php
-require './components/header.php';
 require "../components/header.php";
 echo '
 <html>
     <head>
 <meta charset="UTF-8">
 <title>Dashboard</title>
+
 </head>
 <header>
-' . getHeader("energy") . '
+' . getHeader("Energy") . '
 </header>
 <body>
-    <form name="myform" action="../database/operations.php">
+    <form name="myform" action="../database/operations.php" method="post">
     <input type="hidden" name="type" value="energy">
         <div>
             <table id="table" width="949" height="700" background="power.png" border="0"  >
@@ -34,26 +34,33 @@ echo '
                 </tr>
                 <tr>
                     <td width="409" height="50"></td>
-                    <td width="50"><div>Power:</div></td>
-                <td width="75"><label for="power"></label><input type="text" name="from_value" id="power" style="width:70px; height:20px;" oninput="convert()" ></td>
-                    <td width="15"><div>Sonuç:</div></td>
-                    <td width="100"><label for="result"></label><input type="text" name="to_value" id="result"></td>
+                    <td width="50"></td>
+                <td width="75">
+                <label for="power">Energy:</label>
+                <input type="text" name="from_value" id="power" style="width:200px; height:30px;"  oninput="convert2()" >
+                </td>
+                    <td width="15"></td>
+                    <td width="100">
+                    <label for="result">Sonuç:</label>
+                    <input type="text" style="width:200px; height:30px;" name="to_value" id="result">
+                    </td>
                     <td width="400"></td>
                     </tr>
                     <tr>
                         <td width="409" height="50"></td>
                         <td width="50"></td>
                         <td width="75">
-                            <select id="unit" name="from_unit" size="5" onchange="convert()" class="select">
+                            <select id="unit" name="from_unit" size="5" onchange="convert2()" class="select">
                                 <option value="watts" selected="">Watt</option>
                                 <option value="kilowatts">Exawatt</option>
                                 <option value="megawatts">Petawatt</option>
                                 <option value="joules">Terawatt</option>
                                 <option value="kilojoules">Gigawatt</option>
-                            </select></td>
+                            </select>
+                            </td>
                         <td width="15"></td>
                         <td width="100">
-                            <select id="result-unit" name="to_unit" size="5" onchange="convert()" class="select">
+                            <select id="result-unit" name="to_unit" size="5" onchange="convert2()" class="select">
                                 <option value="watts" selected="">Watt</option>
                                 <option value="kilowatts">Exawatt</option>
                                 <option value="megawatts">Petawatt</option>
@@ -65,7 +72,8 @@ echo '
                     <tr>
                         <td width="409"  height="100"></td>
                         <td width="50"></td>
-                        <td width="75"></td>
+                        <td width="75">
+                    <input type="submit" value="Kaydet" ></td>
                         <td width="15"></td>
                         <td width="100"></td>
                         <td width="400"></td>
@@ -82,28 +90,41 @@ echo '
         </div>
 
     </form>
-<script>
-    function convert() {
+</body>
+
+<script type="text/javascript">
+    function convert2() {
         var power = document.getElementById("power").value;
         var unit = document.getElementById("unit").value;
         var resultUnit = document.getElementById("result-unit").value;
         var resultelement = document.getElementById("result");
-var result = 0;
+        var result = 0;
         
 
-        if (unit == "watts") {
-            if (resultUnit == "watts") {
+        if (unit == "watts") 
+        {
+            if (resultUnit == "watts") 
+            {
                 result = power + " watts";
-            } else if (resultUnit == "kilowatts") {
+            } 
+            else if (resultUnit == "kilowatts") 
+            {
                 result = (power * 0.001);
-            } else if (resultUnit == "megawatts") {
+            } 
+            else if (resultUnit == "megawatts") 
+            {
                 result = (power * 0.000001);
-            } else if (resultUnit == "joules") {
+            }
+            else if (resultUnit == "joules") 
+            {
                 result = (power * 3600) ;
-            } else if (resultUnit == "Kilojoule") {
+            }
+            else if (resultUnit == "Kilojoule") 
+            {
                 result = (power * 3.6);
             }
-        } else if (unit == "kilowatts") {
+        }
+        else if (unit == "kilowatts") {
             if (resultUnit == "watts") {
                 result =(power * 1000) ;
             } else if (resultUnit == "kilowatts") {
@@ -133,11 +154,11 @@ var result = 0;
             } else if (resultUnit == "kilowatts") {
                 result = (power * 0.0000002777778);
             } else if (resultUnit == "megawatts") {
-                result =,(power * 0.0000000002777778),;
+                result =(power * 0.0000000002777778);
             } else if (resultUnit == "joules") {
-                result = power ,;
+                result = power ;
             } else if (resultUnit == "Kilojoule") {
-                result =,(power * 0.001),;
+                result = (power * 0.001);
             }
         } else if (unit == "Kilojoule") {
             if (resultUnit == "watts") {
@@ -152,10 +173,8 @@ var result = 0;
                 result = power;
             }
         }
-console.log(result);
         document.getElementById("result").value = result;
     }
 </script>
-</body>
 </html>
 ';
